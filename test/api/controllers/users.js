dd1c27b7c595cf01f366187aaeacfed6cc798c91
James Wright <jcwright04@gmail.com>
2016-11-18 22:49:01 -0800

    feat(test): http POST
    add http POST request feature

--- test/api/controllers/users.js
+++ test/api/controllers/users.js
@@ -1,19 +1,29 @@
-let should = require('should');
+let should  = require('should');
 let request = require('supertest');
-let server = require('../../../app');
+let server  = require('../../../app');
 let express = require('express');
 let rNumber = require('genrandom').rNumber;
 
 let app = express();
 
-app.get('/users', function(req, res) {
+app.get('/users', function (req, res) {
     res.status(200).json({
-        id: String(rNumber(15)),
-        firstName: 'tobi',
-        lastName: 'smith',
-        email: 'testing@test.com'
+        id        : String(rNumber(15)),
+        firstName : 'tobi',
+        lastName  : 'smith',
+        email     : 'testing@test.com'
     });
 });
+
+/*app.post('/users', function (req, res) {
+    let userObj = {
+        id : rNumber(15),
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email
+    }
+    res.send(userObj);
+});*/
 
 describe('controllers', () => {
 
@@ -27,7 +37,29 @@ describe('controllers', () => {
                     .expect('Content-Length', '12')
                     .expect(200)
                     .end((err, res) => {
-                        if (err) throw err;
+                        if (err) {
+                            throw err;
+                        }
+                        done();
+                    });
+            });
+        });
+
+        describe('POST Single User /users', () => {
+            it('should post a single user', (done) => {
+                request(server)
+                    .post('/users')
+                    .expect('Content-Type', /json/)
+                    .expect('Content-Length', '40')
+                    .field('id', rNumber(15))
+                    .field('firstName', "Jane")
+                    .field('lastName', "Doe")
+                    .field('email', "testinguser@testing.com")
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) {
+                            throw err;
+                        }
                         done();
                     });
             });
