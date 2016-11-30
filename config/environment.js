6815784a09025063ea99a8ace0b88bc7bd14f3cb
James Wright <jcwright04@gmail.com>
2016-11-29 23:01:12 -0800

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 function environment () {
 
     return {
-        TEST_DB_URL : "mongodb://<db Username>:<db password>@ds115738.mlab.com:15738/<db name>"
+        TEST_DB_URL : "mongodb://swaguser:cs8SjaG3HPjZzFV@ds115738.mlab.com:15738/swaggertestdb"
     }
 }
 
