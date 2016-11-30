674cd5186406b92aaed14c0df4b6e4806b1c6895
James Wright <jcwright04@gmail.com>
2016-11-29 22:51:18 -0800

    chore(environment.js): removed environment.js from .gitignore

---
+++ config/environment.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function environment () {
+
+    return {
+        TEST_DB_URL : "mongodb://swaguser:cs8SjaG3HPjZzFV@ds115738.mlab.com:15738/swaggertestdb"
+    }
+}
+
+module.exports = {
+
+    environment : environment
+}
